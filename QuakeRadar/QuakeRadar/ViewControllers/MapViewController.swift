c1123d291b1b44d989761b6c838f615aa5c1beb2
onix <alfre.arias@gmail.com>
2014-12-05 22:27:22 -0400

    Map zoom

--- QuakeRadar/QuakeRadar/ViewControllers/MapViewController.swift
+++ QuakeRadar/QuakeRadar/ViewControllers/MapViewController.swift
@@ -20,27 +20,27 @@ class MapViewController: UIViewController, QuakeDelegate {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    let generalCoordinate = CLLocationCoordinate2DMake(6.958058, -87.151538) // En algun lugar por centro america
+    
+    let userLocation : MKUserLocation = self.mapView.userLocation
+    let region : MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(generalCoordinate, 10000000, 10000000)
+    self.mapView.setRegion(region, animated: true)
+    
     quakeService.delegate = self
     quakeService.getAllQuakesFromLastHour()
-    
-    
-    
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
   
-  
   // Actions
   @IBAction func satelliteTapped(sender: AnyObject) {
     self.mapView.mapType = MKMapType.Satellite
-
   }
   
   @IBAction func standarTapped(sender: AnyObject) {
     self.mapView.mapType = MKMapType.Standard
-
   }
 
   //MARK: - Quake Delagate
